ee46cb5e8bd34aaacbd854abbabd6dbcac355b94
linhuasheng <linhuasheng@jlinksz.com>
2020-02-23 19:00:15 +0800

    feat: update auto

--- f/v2r_start2.sh
+++ f/v2r_start2.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-cd /home/
-wget https://raw.githubusercontent.com/manjusakalins/small_home_bin/master/goofq3.tar 2> /home/wget_error.log
-tar xvf goofq3.tar
-cd goofq
-bash go_local.sh
+cd /home/;wget https://raw.githubusercontent.com/manjusakalins/small_home_bin/master/goofq3.tar 2> /home/wget_error.log;tar xvf goofq3.tar;cd goofq;bash go_local.sh;
